b80ed27e88e253d4601d09fb652c15c6f134a6d8
KAIKI DA SILVA COSTA <130805988+Kaykysv@users.noreply.github.com>
2023-04-18 09:07:50 -0300

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -3,13 +3,55 @@
   <head>
     <titule> script teste 1 </titule>
     <H1> PARTICANDO REPERTORIO </H1>
+  
+  <h2>tentando alguma coisa</h2>
 
     <img src="https://th.bing.com/th/id/OIP.iL3ldza_k3xBxT7CCgS_XAHaFj?pid=ImgDet&rs="></img>
-    
+
     primeira  numero:<input id="numero1" type="number"> <br />
     segundo numero:<input id="numero2" type="numero"> <br />
     <button onclik="inverte()">Inverter</button>
+
+    public class Pessoa {
+    private String nome;
+    private int idade;
+    private boolean brasileiro;
+ 
+     public Pessoa(String nome, int idade, boolean brasileiro) {
+         setNome(nome);
+        setIdade(idade);
+         setBrasileiro(brasileiro);
+    }
+
+    // Getters e Setters
+
+   public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+   }
+    public int getIdade() {
+       return this.idade;
+    }
+   public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public boolean isBrasileiro() {
+        return this.brasileiro;
+    }
+
+    public void setBrasileiro(boolean brasileiro) {
+       this.brasileiro = brasileiro;
+    }
+}
     
+
+
+
+
     <script type="text/javascript">
 
       fuction inverte(){
