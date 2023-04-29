7509d4fb1af4f9e9c467358b0a1d8b7e46a41ea4
KAIKI DA SILVA COSTA <130805988+Kaykysv@users.noreply.github.com>
2023-04-28 22:01:35 -0300

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,10 +1,23 @@
 <!DOCTYPE html>
+
 <html>
+
   <head>
     <titule> script teste 1 </titule>
+
     <H1> PARTICANDO REPERTORIO </H1>
+
+    <h2>Todo mundo que tem um conhecimento, mesmo que básico, sobre o funcionamento dos sites, já ouviu falar sobre JavaScript. Essa linguagem de scripts foi criada para dar mais dinamismo aos sites e, graças à sua versatilidade, extensa comunidade, variedade de bibliotecas e frameworks, tornou-se uma solução para os diversos tipos de aplicações.
+
+O desenvolvedor especialista nessa linguagem transforma-se em um profissional completo. Um dos principais responsáveis pela mudança de patamar do JavaScript, de uma linguagem client-side para uma solução integral, foi o interpretador Node.js, que permitiu a sua utilização também do lado do servidor e viabilizou a criação de sites completos.
+
+Neste post, vamos conhecer o que é a linguagem JavaScript, suas aplicações, principais bibliotecas e frameworks e por que ela está crescendo cada vez mais. Confira!Todo mundo que tem um conhecimento, mesmo que básico, sobre o funcionamento dos sites, já ouviu falar sobre JavaScript. Essa linguagem de scripts foi criada para dar mais dinamismo aos sites e, graças à sua versatilidade, extensa comunidade, variedade de bibliotecas e frameworks, tornou-se uma solução para os diversos tipos de aplicações.
+
+O desenvolvedor especialista nessa linguagem transforma-se em um profissional completo. Um dos principais responsáveis pela mudança de patamar do JavaScript, de uma linguagem client-side para uma solução integral, foi o interpretador Node.js, que permitiu a sua utilização também do lado do servidor e viabilizou a criação de sites completos.
+
+Neste post, vamos conhecer o que é a linguagem JavaScript, suas aplicações, principais bibliotecas e frameworks e por que ela está crescendo cada vez mais. Confira!</h2>
   
-  <h2>tentando alguma coisa</h2>
+  <h3>tentando alguma coisa</h3>
 
     <img src="https://th.bing.com/th/id/OIP.iL3ldza_k3xBxT7CCgS_XAHaFj?pid=ImgDet&rs="></img>
 
@@ -21,36 +34,37 @@
          setNome(nome);
         setIdade(idade);
          setBrasileiro(brasileiro);
-    }
+    {"}"}
 
     // Getters e Setters
 
    public String getNome() {
         return this.nome;
-    }
-
+    {"}"}do {
+      
+  
+    } while (do {
+      condition
+    } while (condition););
     public void setNome(String nome) {
         this.nome = nome;
-   }
+   {"}"}
     public int getIdade() {
        return this.idade;
-    }
+    {"}"}
    public void setIdade(int idade) {
         this.idade = idade;
-    }
+    {"}"}
 
     public boolean isBrasileiro() {
         return this.brasileiro;
-    }
+    {"}"}
 
     public void setBrasileiro(boolean brasileiro) {
        this.brasileiro = brasileiro;
-    }
-}
+    {"}"}
+{"}"}
     
-
-
-
 
     <script type="text/javascript">
 
@@ -68,8 +82,79 @@
         "Invertido:\n"+
         "numero1="+num1+"n"+
         "numero2="+num2+"\n\n");
-      }
+      {"}"}
     </script>
 
   </head>
-</html>
+
+<body>
+
+img src="https://th.bing.com/th/id/OIP.iL3ldza_k3xBxT7CCgS_XAHaFj?pid=ImgDet&rs="{">"}</img>
+
+    primeira  numero:<do {
+      input
+    } while (condition); id="numero1" type="number"> <br />
+    segundo numero:<input id="numero2" type="numero"> <br />
+    <button onclik="inverte()">Inverter</button>
+
+    public class Pessoa {
+    private String nome;
+    private int idade;
+    private boolean brasileiro;
+ 
+     public Pessoa(String nome, int idade, boolean brasileiro) {
+         setNome(nome);
+        setIdade(idade);
+         setBrasileiro(brasileiro);
+    {"}"}
+
+    // Getters e Setters
+
+   public String getNome() {
+        return this.nome;
+    {"}"}
+
+    public void setNome(String nome) {
+        this.nome = nome;
+   {"}"}
+    public int getIdade() {
+       return this.idade;
+    {"}"}
+   public void setIdade(int idade) {
+        this.idade = idade;
+    {"}"}
+
+    public boolean isBrasileiro() {
+        return this.brasileiro;
+    {"}"}
+
+    public void setBrasileiro(boolean brasileiro) {
+       this.brasileiro = brasileiro;
+    {"}"}
+{"}"}
+    
+
+    <script type="text/javascript">
+
+      fuction inverte(){
+        var num1 = parseFloat(document.getElementById("numero1").value);
+        var nnum2 = parseFloat(document.getElementById("numero2").value);
+
+        var aux = num1
+        num1=num2;
+        nnum2=aux;
+
+        alert("Antes:\n"+
+        "numero1="+document.getElementById("numero1").value+"\n"+
+        "numero2="+document.getElementById("numeero2").value+"\n\n"+
+        "Invertido:\n"+
+        "numero1="+num1+"n"+
+        "numero2="+num2+"\n\n");
+      {"}"}
+
+</body>
+do {
+  
+  </html>
+
+{"}"} while (condition);
